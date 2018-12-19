18fec91d2b12be74f367ce9eada36ba205610d88
Erik Bosman <erik@minemu.org>
2018-12-19 11:51:28 +0100

    add symbol helper scripts

---
+++ script/get_fileoffset_from_virtual.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+get_file_offset()
+{
+	#SYM="$(readelf -sW "$1" | grep "$2"'$' |awk '{print $2}')"
+	SYM="$2"
+	readelf -lW "$1" | grep '  LOAD' | awk '{printf ("x=0x'"$SYM"'\nif %s <= x < %s+%s: print \"%%x\" %% (x+%s-%s)\n", $3,$3,$5,$2,$3)}' |python
+}
+
+resolve_hook()
+{
+	OFFSET=$(get_file_offset "$1" "$2")
+	echo "$1" "$OFFSET"
+}
+
+resolve_hook "$1" "$2"
+
