88e9f68d69bd4ef30c6923f4bf9978388fb4c1d3
Erik Bosman <erik@minemu.org>
2018-12-23 20:31:34 +0100

    ignore symbol info in symbol script

--- script/get_fileoffset_from_symbol.sh
+++ script/get_fileoffset_from_symbol.sh
@@ -2,7 +2,7 @@
 
 get_file_offset()
 {
-	SYM="$(readelf -sW "$1" | grep "$2"'$' |awk '{print $2}')"
+	SYM="$(readelf -sW "$1" | grep " $2"'@@' |awk '{print $2}')"
 	readelf -lW "$1" | grep '  LOAD' | awk '{printf ("x=0x'"$SYM"'\nif %s <= x < %s+%s: print \"%%x\" %% (x+%s-%s)\n", $3,$3,$5,$2,$3)}' |python
 }
 
